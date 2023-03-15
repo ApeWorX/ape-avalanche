e4df22d82b6a40c612fa0e6ac7187cc0f6f80bb6
PoCk3T-SPAI <125048016+PoCk3T-SPAI@users.noreply.github.com>
2023-03-14 19:11:29 -0700

    feat: support for Fuji testnet of Avalanche [APE-714] (#4)
    * Update __init__.py
    
    * Update ecosystem.py
    
    * Update test_integration.py

--- ape_avalanche/ecosystem.py
+++ ape_avalanche/ecosystem.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from ape.api.config import PluginConfig
 from ape.api.networks import LOCAL_NETWORK_NAME
 from ape_ethereum.ecosystem import Ethereum, NetworkConfig
@@ -7,28 +5,14 @@ from ape_ethereum.ecosystem import Ethereum, NetworkConfig
 NETWORKS = {
     # chain_id, network_id
     "mainnet": (43114, 43114),
+    "fuji": (43113, 43113),
 }
 
 
-def _create_network_config(
-    required_confirmations: int = 1, block_time: int = 1, **kwargs
-) -> NetworkConfig:
-    # Helper method to isolate `type: ignore` comments.
-    return NetworkConfig(
-        required_confirmations=required_confirmations, block_time=block_time, **kwargs
-    )  # type: ignore
-
-
-def _create_local_config(default_provider: Optional[str] = None) -> NetworkConfig:
-    return _create_network_config(
-        required_confirmations=0, block_time=0, default_provider=default_provider
-    )
-
-
 class AvalancheConfig(PluginConfig):
-    mainnet: NetworkConfig = _create_network_config()
-    mainnet_fork: NetworkConfig = _create_local_config()
-    local: NetworkConfig = _create_local_config(default_provider="test")
+    mainnet: NetworkConfig = NetworkConfig(required_confirmations=1, block_time=2)  # type: ignore
+    fuji: NetworkConfig = NetworkConfig(required_confirmations=1, block_time=2)  # type: ignore
+    local: NetworkConfig = NetworkConfig(default_provider="test")  # type: ignore
     default_network: str = LOCAL_NETWORK_NAME
 
 
