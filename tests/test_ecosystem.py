55c94f28a87fc36ef0b637138343920cc0f42201
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2023-12-18 18:42:48 -0500

    feat!: Handle 0.7 breaking changes and updates [APE-1550] (#12)

--- tests/test_ecosystem.py
+++ tests/test_ecosystem.py
@@ -20,7 +20,7 @@ def test_create_transaction(avalanche, tx_type, eth_tester_provider):
     (TransactionType.STATIC.value, TransactionType.DYNAMIC.value),
 )
 def test_encode_transaction(tx_type, avalanche, eth_tester_provider):
-    abi = MethodABI.parse_obj(
+    abi = MethodABI.model_validate(
         {
             "type": "function",
             "name": "fooAndBar",
